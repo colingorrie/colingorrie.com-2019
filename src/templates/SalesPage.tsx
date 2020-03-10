6328031450fc77547c8b61db1b08262de8bc2f2a
Colin Gorrie <colin@colingorrie.com>
2020-03-10 08:38:12 -0400

    Add content to SalesPage

--- src/templates/SalesPage.tsx
+++ src/templates/SalesPage.tsx
@@ -10,6 +10,19 @@ type Props = {
     airtablePages: {
       data: {
         title: string;
+        content: {
+          data: {
+            pain: string;
+            dream: string;
+            fix: string;
+            cta1: string;
+            socialProof: string;
+            overcomeObjections: string;
+            uniqueness: string;
+            cta2: string;
+            urgency: string;
+          };
+        }[];
       };
       fields: {
         slug: string;
@@ -20,11 +33,23 @@ type Props = {
 
 const SalesPage: FunctionComponent<Props> = ({ data, location }) => {
   const { airtablePages: page } = data;
+  const { title, content } = page.data;
 
   return (
-    <Layout location={location} title={page.data.title}>
-      <SEO title={page.data.title} />
-      <h1>{page.data.title}</h1>
+    <Layout location={location} title={title}>
+      <SEO title={title} />
+      <h1>{title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: content[0].data.pain }} />
+      <div dangerouslySetInnerHTML={{ __html: content[0].data.dream }} />
+      <div dangerouslySetInnerHTML={{ __html: content[0].data.fix }} />
+      <div dangerouslySetInnerHTML={{ __html: content[0].data.cta1 }} />
+      <div dangerouslySetInnerHTML={{ __html: content[0].data.socialProof }} />
+      <div
+        dangerouslySetInnerHTML={{ __html: content[0].data.overcomeObjections }}
+      />
+      <div dangerouslySetInnerHTML={{ __html: content[0].data.uniqueness }} />
+      <div dangerouslySetInnerHTML={{ __html: content[0].data.cta2 }} />
+      <div dangerouslySetInnerHTML={{ __html: content[0].data.urgency }} />
     </Layout>
   );
 };
@@ -38,6 +63,19 @@ export const pageQuery = graphql`
       data {
         name
         title
+        content {
+          data {
+            pain
+            dream
+            fix
+            cta1
+            socialProof
+            overcomeObjections
+            uniqueness
+            cta2
+            urgency
+          }
+        }
       }
       fields {
         slug
