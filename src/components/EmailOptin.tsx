9996f6c0dc6ceddb7c8dc167fadcd5fd0d83b4d5
Colin Gorrie <colin@colingorrie.com>
2020-03-11 17:58:15 -0400

    Use netlify form

--- src/components/EmailOptin.tsx
+++ src/components/EmailOptin.tsx
@@ -6,45 +6,47 @@ type Props = {
 };
 
 export const EmailOptin: FunctionComponent<Props> = ({ idPrefix }) => {
-  const { register, handleSubmit, errors } = useForm();
-  const onSubmit = data => console.log(data);
+  const { register } = useForm();
 
   const firstNameId = `${idPrefix}-first-name`;
   const emailId = `${idPrefix}-email`;
 
   return (
     <form
-      onSubmit={handleSubmit(onSubmit)}
-      className="flex flex-col mb-8 bg-gray-300 p-4"
+      name="newsletter"
+      data-netlify="true"
+      netlify-honeypot="bot-field"
+      className="flex flex-col mb-8 bg-gray-300 p-4 shadow-sm"
     >
-      <label htmlFor={firstNameId} className="flex flex-col mb-1">
+      <input type="hidden" name="form-name" value="newsletter" />
+      <label htmlFor={firstNameId} className="flex flex-col mb-1 text-gray-800">
         First name
         <input
           id={firstNameId}
           type="text"
-          placeholder="Enter your first name here"
+          placeholder="Your first name"
           name="First name"
           required
           ref={register({ required: true, maxLength: 80 })}
-          className="w-64 mb-2 border-2 border-solid border-gray-500 py-1 px-2"
+          className="w-64 mb-2 border-2 border-solid border-gray-400 py-1 px-2"
         />
       </label>
-      <label htmlFor={emailId} className="flex flex-col mb-1">
+      <label htmlFor={emailId} className="flex flex-col mb-1 text-gray-800">
         Email
         <input
           id={emailId}
           type="text"
-          placeholder="your.email@example.com"
+          placeholder="Your email address"
           name="Email"
           required
           ref={register({ required: true, pattern: /^\S+@\S+$/i })}
-          className="w-64 mb-2 border-2 border-solid border-gray-500 py-1 px-2"
+          className="w-64 mb-2 border-2 border-solid border-gray-400 py-1 px-2"
         />
       </label>
 
       <input
         type="submit"
-        className="w-24 py-1 border-solid text-white bg-teal-700 shadow-sm border-t-2 border-teal-600 mt-2"
+        className="w-24 py-1 border-solid text-white bg-teal-700 shadow-md border-t-2 border-teal-600 my-2"
       />
     </form>
   );
