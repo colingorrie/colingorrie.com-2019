f2f9f850e35ab65d1fe90e136eb53b13ef22dd02
Colin Gorrie <colin@colingorrie.com>
2020-04-13 20:43:57 -0400

    Add page title

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Link } from 'gatsby';
+import { Link, useStaticQuery, graphql } from 'gatsby';
 
 type Props = {
   location: {
@@ -9,32 +9,53 @@ type Props = {
 };
 
 const Layout: FunctionComponent<Props> = ({ location, title, children }) => {
+  const data = useStaticQuery(graphql`
+    query LayoutQuery {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+
+  const { site } = data;
+  const { siteMetadata } = site;
+  const { title: siteTitle } = siteMetadata;
+
   const rootPath = `${__PATH_PREFIX__}/`;
-  let header;
+  let SiteTitle;
 
   if (location.pathname === rootPath) {
-    header = (
+    SiteTitle = (
       <h1 className="site-title">
         <Link className="shadow-none no-underline" to={`/`}>
-          {title}
+          {siteTitle}
         </Link>
       </h1>
     );
   } else {
-    header = (
+    SiteTitle = (
       <h3 className="site-title">
         <Link className="shadow-none no-underline" to={`/`}>
-          {title}
+          {siteTitle}
         </Link>
       </h3>
     );
   }
   return (
     <div className="mx-auto max-w-xl px-6 md:px-0 py-10 flex flex-col min-h-screen">
+      {location.pathname !== rootPath && (
+        <header>
+          <h1 className="page-title">
+            <span>{title}</span>
+          </h1>
+        </header>
+      )}
       <main className="flex-grow">{children}</main>
       <footer className="self-center text-center text-gray-700">
         <nav>
-          <div>{header}</div>
+          <div>{SiteTitle}</div>
           <ul className="flex flex-row justify-center">
             {/* <li className="mx-4">
               <a href="/" className="link">
